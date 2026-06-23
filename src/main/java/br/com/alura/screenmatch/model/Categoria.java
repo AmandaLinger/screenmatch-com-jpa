e58e31aa58d705813c2a67fb421335c5e24dd1a8
Amanda Marques <amandamarqueslinger@gmail.com>
2026-06-23 08:57:00 -0300

    implementando novos métodos

--- src/main/java/br/com/alura/screenmatch/model/Categoria.java
+++ src/main/java/br/com/alura/screenmatch/model/Categoria.java
@@ -1,22 +1,34 @@
 package br.com.alura.screenmatch.model;
 
 public enum Categoria {
-    ACAO("Action"), //criando o tipo da categoria e passando o valor correspondente
-    ROMANCE("Romance"),
-    COMEDIA("Comedy"),
-    DRAMA("Drama"),
-    CRIME("Crime");
+    ACAO("Action", "Ação"), //criando o tipo da categoria e passando o valor correspondente
+    ROMANCE("Romance", "Romance"),
+    COMEDIA("Comedy", "Comédia"),
+    DRAMA("Drama", "Drama"),
+    CRIME("Crime", "Crime");
 
     private String categoriaOmdb;
+    private String categoriaEmPortugues;
 
-    Categoria(String categoriaOmdb) {
+    Categoria(String categoriaOmdb, String categoriaEmPortugues) {
+
         this.categoriaOmdb = categoriaOmdb;
+        this.categoriaEmPortugues = categoriaEmPortugues;
     }
 
 
     public static Categoria fromString(String text) { //interpreta o valor do ombd e converte para as categorias listadas acima
         for (Categoria categoria : Categoria.values()) {
             if (categoria.categoriaOmdb.equalsIgnoreCase(text)) {
+                return categoria;
+            }
+        }
+        throw new IllegalArgumentException("Nenhuma categoria encontrada para a string fornecida: " + text);
+    }
+
+    public static Categoria fromPortugues(String text) { //interpreta o valor do ombd e converte para as categorias listadas acima
+        for (Categoria categoria : Categoria.values()) {
+            if (categoria.categoriaEmPortugues.equalsIgnoreCase(text)) {
                 return categoria;
             }
         }
--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -1,9 +1,6 @@
 package br.com.alura.screenmatch.principal;
 
-import br.com.alura.screenmatch.model.DadosSerie;
-import br.com.alura.screenmatch.model.DadosTemporada;
-import br.com.alura.screenmatch.model.Episodio;
-import br.com.alura.screenmatch.model.Serie;
+import br.com.alura.screenmatch.model.*;
 import br.com.alura.screenmatch.repository.SerieRepository;
 import br.com.alura.screenmatch.service.ConsumoApi;
 import br.com.alura.screenmatch.service.ConverteDados;
@@ -39,6 +36,7 @@ public class Principal {
                     4 - Buscar série por título
                     5 - Buscar séries por ator
                     6 - Top 5 séries
+                    7 - Buscar séries por categoria
                     
                     0 - Sair                                 
                     """;
@@ -68,6 +66,9 @@ public class Principal {
                     break;
                 case 6:
                     buscarTop5Series();
+                    break;
+                case 7:
+                    buscarSeriePorCategoria();
                     break;
                 case 0:
                     System.out.println("Saindo...");
@@ -158,4 +159,16 @@ public class Principal {
         List<Serie> serieTop = repositorio.findTop5ByOrderByAvaliacaoDesc();
         serieTop.forEach(s -> System.out.println(s.getTitulo() + " avaliação: " + s.getAvaliacao()));
     }
+
+
+    private void buscarSeriePorCategoria() {
+        System.out.println("Qual categoria deseja buscar?");
+        var categoriaEscolhida = leitura.nextLine();
+        Categoria categoria = Categoria.fromPortugues(categoriaEscolhida);
+        List<Serie> seriesPorCategoria = repositorio.findByGenero(categoria);
+        System.out.println("Séries da categoria: " + categoriaEscolhida);
+        seriesPorCategoria.forEach(System.out::println);
+
+    }
+
 }
